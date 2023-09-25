996f34751a2f5ddab1ecbe52bd00deab90923776
Moises Alonso <maalonso@uvg.edu.gt>
2023-09-25 07:25:31 -0600

    Ultima version

--- 10_Herencia/src/edu/uvg/portal/view/ConsoleUI.java
+++ 10_Herencia/src/edu/uvg/portal/view/ConsoleUI.java
@@ -6,6 +6,8 @@ package edu.uvg.portal.view;
 import java.util.Scanner;
 
 import edu.uvg.portal.controller.Validation;
+import edu.uvg.portal.model.Docente;
+import edu.uvg.portal.model.Estudiante;
 import edu.uvg.portal.model.User;
 
 /**
@@ -32,6 +34,25 @@ public class ConsoleUI {
 		
 		if (usuario != null) { //Si lo encontro con las credenciales correctas
 			usuario.showMenu();
+			switch(usuario.getType()) {
+			
+			case 0 :{ //Estudiante
+				
+				//Hago que usuario sea un estudiante
+				Estudiante miEstudiante = (Estudiante)usuario;
+				miEstudiante.getNotas(); //Este metodo solo es de esutidante
+				
+			}break;
+			
+			case 1:{ //Docente
+				
+				//Hago que el usuario sea un docente
+				Docente miDocente = (Docente)usuario;
+				miDocente.calificar(null, 0); //Este metodo solo es de docente
+				
+			}break;
+			
+			}
 		} else { //Fallo credenciales o usuario
 			System.out.println("Fallo credenciales");
 		}
